c798c62334c084fe8b1cd8d8052a7f9ffb621ba0
jontama <whsxkak@gmail.com>
2023-02-22 00:22:01 +0900

    테스트

---
+++ src/main/java/com/greglturnquist/hackingspringbootch2reactive/StSpringBootReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringbootch2reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StSpringBootReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StSpringBootReactiveApplication.class, args);
+	}
+
+}
